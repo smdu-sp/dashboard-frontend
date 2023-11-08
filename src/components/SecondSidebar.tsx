4f19fe09c38fe68f0465fbff8eeecd38f87cf75d
Victor Abreu <aalevictor@gmail.com>
2023-11-08 10:56:15 -0300

    inicial

---
+++ src/components/SecondSidebar.tsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import { Link, ListItemButton, ListItemDecorator, ListItemContent, ListItem, List, Sheet, Box } from '@mui/joy';
+import { AppRegistration, Home } from '@mui/icons-material';
+
+import { closeSidebar } from '../utils';
+
+const pages = [
+  {
+    title: 'Página Inicial',
+    href: '/',
+    name: '/',
+    icon: <Home />,
+  },
+  {
+    title: 'Cadastro',
+    href: '/cadastro',
+    name: '/cadastro',
+    icon: <AppRegistration />,
+  },
+];
+
+export default function SecondSidebar({
+  pagina
+} : {
+  pagina?: string;
+}) {
+  return (
+    <React.Fragment>
+      <Box
+        className="SecondSidebar-overlay"
+        sx={{
+          position: 'fixed',
+          zIndex: 99,
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          opacity: 'var(--SideNavigation-slideIn)',
+          backgroundColor: 'var(--joy-palette-background-backdrop)',
+          transition: 'opacity 0.4s',
+          transform: {
+            xs: 'translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1) + var(--SideNavigation-slideIn, 0) * var(--FirstSidebar-width, 0px)))',
+            lg: 'translateX(-100%)',
+          },
+        }}
+        onClick={() => closeSidebar()}
+      />
+      <Sheet
+        className="SecondSidebar"
+        color="neutral"
+        sx={{
+          position: {
+            xs: 'fixed',
+            lg: 'sticky',
+          },
+          transform: {
+            xs: 'translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1) + var(--SideNavigation-slideIn, 0) * var(--FirstSidebar-width, 0px)))',
+            lg: 'none',
+          },
+          transition: 'transform 0.4s',
+          zIndex: 99,
+          height: '100dvh',
+          top: 0,
+          p: 2,
+          flexShrink: 0,
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+          borderRight: '1px solid',
+          borderColor: 'divider',
+        }}
+      >
+        <List
+          size="sm"
+          sx={{
+            '--ListItem-radius': '6px',
+            '--List-gap': '6px',
+          }}
+        >
+          {pages.map((page) => (
+            <Link 
+              href={page.href}
+              underline='none'
+            >
+              <ListItem sx={{ width: '100%'}}>
+                  <ListItemButton selected={pagina===page.name}>
+                    <ListItemDecorator>
+                      {page.icon}
+                    </ListItemDecorator>
+                    <ListItemContent>{page.title}</ListItemContent>
+                  </ListItemButton>
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+      </Sheet>
+    </React.Fragment>
+  );
+}
