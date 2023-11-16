95a241dee4581c23b1415458fbbc99c8c9eb736e
Victor Abreu <aalevictor@gmail.com>
2023-11-16 15:44:33 -0300

    pagina inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 'use client'
 
-import Table from '@mui/joy/Table';
-import Typography from '@mui/joy/Typography';
-import Sheet from '@mui/joy/Sheet';
 import Content from '@/components/Content';
-import { Box, FormControl, FormLabel, Input, Select, IconButton, Option, Button } from '@mui/joy';
-import { KeyboardArrowLeft, KeyboardArrowRight, Search } from '@mui/icons-material';
-
 
 export default function Home() {
   
@@ -15,27 +9,6 @@ export default function Home() {
       breadcrumbs={[]}
       titulo='Página Inicial'
       pagina='/'
-    >
-      <Box
-        className="SearchAndFilters-tabletUp"
-        sx={{
-          borderRadius: 'sm',
-          py: 2,
-          display: {
-            xs: 'none',
-            sm: 'flex',
-          },
-          flexWrap: 'wrap',
-          gap: 1.5,
-          '& > *': {
-            minWidth: {
-              xs: '120px',
-              md: '160px',
-            },
-          },
-        }}
-      >
-      </Box>      
-    </Content>
+    ></Content>
   );
 }
