ef933d12822163b17bfa09e687c484cec6c88628
Gustavo08Mendes <gustavo08.mendes@gmail.com>
2024-05-29 16:14:01 -0300

    Corrigido bug dark mode

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -7,24 +7,28 @@ import { useContext, useEffect, useState } from "react";
 
 export default function ThemeToggle({ ...props }) {
   const [joymode, setJoymode] = useState("light");
+  const [bug, setConfirmaDark] = useState(true);
   const { mode, toggleMode } = useContext(ThemeContext);
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const joymode = localStorage.getItem("joy-mode");
-      console.log(joymode);
-      setJoymode(joymode || "light");
-    }    
-  }, []);
+    const joymode = localStorage.getItem("joy-mode");
+    setJoymode(joymode || "light");
+  }, [toggleMode, joymode]);
+  window.onload = () => {
+    if (joymode === "dark" && bug === true) {
+      toggleMode()
+      setConfirmaDark(false);
+    }
+  }
   return (
     <IconButton
       id="toggle-mode"
       size="sm"
       color="primary"
       sx={{ p: 0.5 }}
-      onClick={() => toggleMode()}
+      onClick={() => { toggleMode(); setJoymode(joymode === "light" ? "dark" : "light"); }}
       {...props}
     >
-      <SvgIcon component={joymode === 'light' ? WbSunny : Nightlight} />
+      <SvgIcon component={joymode == 'light' ? WbSunny : Nightlight} />
     </IconButton>
   );
 }
